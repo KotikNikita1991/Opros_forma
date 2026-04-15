94356837cc11c0f971a63a93e8ae5fd5d1832d80
KotikNikita1991 <mcnil1991@gmail.com>
2026-04-15 13:49:17 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.PVQ_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxjmPL5FS2kHh3eeYUmy_DFii3h6vJH8ETC7aj9V7gzRI3IeSTMfUuoFUKWZadUPNuf_w/exec'
+  API_URL: 'https://vacancy-app-proxy.mcnil1991.workers.dev'
 };
