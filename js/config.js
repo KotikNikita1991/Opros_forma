13a847e2d949277a873baa0b1581f9067dca8776
KotikNikita1991 <mcnil1991@gmail.com>
2026-04-22 16:30:18 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.PVQ_CONFIG = {
   // Основной backend: Google Apps Script (обычно доступен без VPN).
-  API_URL: 'https://script.google.com/macros/s/AKfycbw7auhv_YALBiENojcJA34H-F6e7LutUIccL4YsPrw4pVo01Wg67S39DZdK242hFatZuQ/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzLhkLDGIfcpkggZ8URjbALtB_kOZGMdwTEptD3OimLuSVk0kDk3xQuHNzuziY77_k-bw/exec',
   // Резервные endpoints. Клиент попробует их по порядку, если основной недоступен.
   API_URL_FALLBACKS: [
     'https://vacancy-app-proxy.mcnil1991.workers.dev'
